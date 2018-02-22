0e6e05aeab4977560e06d95502ec9da8ba9ae7ea
maksimlysenko <maxim.lysenko@genesys.com>
2018-02-22 19:10:30 +0000

    9.0.000.00.857

--- src/main/java/com/genesys/internal/authentication/model/ResponseStatus.java
+++ src/main/java/com/genesys/internal/authentication/model/ResponseStatus.java
@@ -26,7 +26,7 @@ import java.io.IOException;
 /**
  * ResponseStatus
  */
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-02-22T17:04:55.709Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-02-22T19:04:38.959Z")
 public class ResponseStatus {
   @SerializedName("code")
   private Integer code = null;
--- src/main/java/com/genesys/internal/common/auth/OAuth.java
+++ src/main/java/com/genesys/internal/common/auth/OAuth.java
@@ -18,7 +18,7 @@ import com.genesys.internal.common.Pair;
 import java.util.Map;
 import java.util.List;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-02-22T17:04:55.709Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-02-22T19:04:38.959Z")
 public class OAuth implements Authentication {
   private String accessToken;
 
