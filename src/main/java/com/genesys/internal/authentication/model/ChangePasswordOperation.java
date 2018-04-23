cc64d369295abbd6a722980046dbfb94fad3d00a
maksimlysenko <maxim.lysenko@genesys.com>
2018-04-23 16:04:12 +0000

    9.0.000.00.959

--- src/main/java/com/genesys/internal/authentication/model/ChangePasswordOperation.java
+++ src/main/java/com/genesys/internal/authentication/model/ChangePasswordOperation.java
@@ -26,7 +26,7 @@ import java.io.IOException;
 /**
  * ChangePasswordOperation
  */
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-04-23T15:24:51.101Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-04-23T15:55:35.249Z")
 public class ChangePasswordOperation {
   @SerializedName("userName")
   private String userName = null;
--- src/main/java/com/genesys/internal/authentication/model/ResponseStatus.java
+++ src/main/java/com/genesys/internal/authentication/model/ResponseStatus.java
@@ -26,7 +26,7 @@ import java.io.IOException;
 /**
  * ResponseStatus
  */
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-04-23T15:24:51.101Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-04-23T15:55:35.249Z")
 public class ResponseStatus {
   @SerializedName("code")
   private Integer code = null;
