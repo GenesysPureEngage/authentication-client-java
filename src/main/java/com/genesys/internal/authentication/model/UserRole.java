cf9adcb0e207423bf8062068245442da48ba1105
maksimlysenko <maxim.lysenko@genesys.com>
2018-06-14 18:53:20 +0000

    9.0.000.00.1088

--- src/main/java/com/genesys/internal/authentication/model/UserRole.java
+++ src/main/java/com/genesys/internal/authentication/model/UserRole.java
@@ -26,7 +26,7 @@ import java.io.IOException;
 /**
  * UserRole
  */
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-06-13T17:01:50.996Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-06-14T18:24:36.575Z")
 public class UserRole {
   @SerializedName("name")
   private String name = null;
--- src/main/java/com/genesys/internal/common/auth/ApiKeyAuth.java
+++ src/main/java/com/genesys/internal/common/auth/ApiKeyAuth.java
@@ -18,7 +18,7 @@ import com.genesys.internal.common.Pair;
 import java.util.Map;
 import java.util.List;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-06-13T17:01:50.996Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-06-14T18:24:36.575Z")
 public class ApiKeyAuth implements Authentication {
   private final String location;
   private final String paramName;
