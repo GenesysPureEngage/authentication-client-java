67d4b7901e344fcae69af74940f37bc21c521d91
maksimlysenko <maxim.lysenko@genesys.com>
2018-07-10 06:18:23 +0000

    9.0.000.00.1180

--- src/main/java/com/genesys/internal/authentication/model/ErrorResponse.java
+++ src/main/java/com/genesys/internal/authentication/model/ErrorResponse.java
@@ -26,7 +26,7 @@ import java.io.IOException;
 /**
  * ErrorResponse
  */
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-07-09T22:04:46.547Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-07-10T05:35:13.053Z")
 public class ErrorResponse {
   @SerializedName("error")
   private String error = null;
--- src/main/java/com/genesys/internal/authentication/model/ModelApiResponse.java
+++ src/main/java/com/genesys/internal/authentication/model/ModelApiResponse.java
@@ -28,7 +28,7 @@ import java.io.IOException;
 /**
  * ModelApiResponse
  */
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-07-09T22:04:46.547Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-07-10T05:35:13.053Z")
 public class ModelApiResponse {
   @SerializedName("data")
   private Object data = null;
